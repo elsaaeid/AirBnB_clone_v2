c8d737541ee0817f7d8b98711c9e6fad71d432ca
elsaaeid <saidsadaoy>
2024-04-08 02:17:32 -0700

    deploy changes

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,31 +2,33 @@
 
 # Install Nginx if not already installed
 if ! command -v nginx &> /dev/null; then
-    sudo apt-get update
-    sudo apt-get install -y nginx
+    sudo apt update
+    sudo apt install -y nginx
 fi
 
-# Create necessary directories
-sudo mkdir -p /data/web_static/releases/test
-sudo mkdir -p /data/web_static/shared
+# Create necessary directories if they don't exist
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
 
 # Create a fake HTML file for testing
 echo "<html>
-  <head>
-  </head>
-  <body>
+<head>
+</head>
+<body>
     Holberton School
-  </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
+</body>
+</html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
 
-# Create symbolic link
-sudo ln -sf /data/web_static/releases/test /data/web_static/current
+# Create or recreate the symbolic link
+sudo rm -rf /data/web_static/current
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current
 
-# Give ownership of the /data/ folder to the ubuntu user and group
+# Set ownership of the /data/ folder to the ubuntu user and group recursively
 sudo chown -R ubuntu:ubuntu /data/
 
 # Update Nginx configuration
-sudo sed -i 's/^\(\s*location \/ {\)$/\1\n\tlocation \/hbnb_static\/ {\n\t\talias \/data\/web_static\/current\/;\n\t}\n/' /etc/nginx/sites-available/default
+config_file="/etc/nginx/sites-available/default"
+sudo sed -i '/^\s*location \/hbnb_static {/a \        alias /data/web_static/current/;' "$config_file"
 
 # Restart Nginx
 sudo service nginx restart
