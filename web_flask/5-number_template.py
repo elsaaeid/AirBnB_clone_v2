de19e93a8dcddec9363b8ac0c61a1a1824d31537
elsaaeid <saidsadaoy>
2024-04-21 11:17:20 -0700

    deploy changes

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,26 +1,28 @@
 #!/usr/bin/python3
-from flask import Flask, render_template
 """import class Flask, render_template method"""
-
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """displays text"""
+    """ displays text on browser """
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def display_hbnb():
-    """displays text"""
+    """ displays text on browser """
     return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def display_C(text):
-    """displays text (str)"""
+    """displays text
+    Args:
+        text (str): text
+    """
     return 'C %s' % text.replace('_', ' ')
 
 
@@ -28,22 +30,30 @@ def display_C(text):
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def display_python(text):
-    """displays text (str)"""
+    """displays text
+    Args:
+        text (str): text
+    """
     return 'Python %s' % text.replace('_', ' ')
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def display_num(n):
-    """displays int number on browser"""
+    """displays int number
+    """
     return "%d is a number" % n
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def display_HTML(n):
-    """displays int number on browser
-    through this route of HTML page
+    """displays text
+    Args:
+        n (int): number
+    Returns:
+        HTML page
     """
     return render_template('5-number.html', n=n)
+
 
 
 if __name__ == "__main__":
