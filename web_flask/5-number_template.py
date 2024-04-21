d6b1c4e405f9c8c8ab08988663cb173769c76b04
elsaaeid <saidsadaoy>
2024-04-21 11:34:57 -0700

    deploy changes

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -7,13 +7,19 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """ displays text on browser """
+    """displays text
+    on browser
+    """
     return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def display_hbnb():
-    """ displays text on browser """
+    """displays text
+    on browser
+    """
     return "HBNB"
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def display_C(text):
@@ -22,6 +28,7 @@ def display_C(text):
         text (str): text
     """
     return 'C %s' % text.replace('_', ' ')
+
 
 @app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
@@ -33,11 +40,13 @@ def display_python(text):
     """
     return 'Python %s' % text.replace('_', ' ')
 
+
 @app.route('/number/<int:n>', strict_slashes=False)
 def display_num(n):
     """displays int number
     """
     return "%d is a number" % n
+
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def display_HTML(n):
@@ -48,5 +57,7 @@ def display_HTML(n):
         HTML page
     """
     return render_template('5-number.html', n=n)
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
