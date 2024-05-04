e92f3fcfce6171249098322a56f3cd9d486b7273
elsaaeid <saidsadaoy@gmail.com>
2024-05-04 18:17:08 +0200

    deploy changes

--- models/base_model.py
+++ models/base_model.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import json
 import os
 from datetime import datetime
@@ -5,13 +6,13 @@ import uuid
 from hashlib import md5
 from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
-import models  # Assuming models is a module in your application
+import models
 
-# Check the storage type and define the base class accordingly
 if models.storage_type == "db":
     Base = declarative_base()
 else:
     Base = object
+
 
 class BaseModel(Base):
     """Base model that all classes will inherit from"""
@@ -22,8 +23,8 @@ class BaseModel(Base):
     updated_at = Column(DateTime, default=datetime.utcnow)
     password = Column(String(128))
 
-    # Constructor to initialize the base model
     def __init__(self, *args, **kwargs):
+        """Initialization of the base model"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.utcnow()
         self.updated_at = self.created_at
@@ -34,23 +35,26 @@ class BaseModel(Base):
         if 'password' in kwargs:
             self.password = md5(kwargs['password'].encode()).hexdigest()
         if 'created_at' in kwargs:
-            self.created_at = datetime.strptime(kwargs['created_at'], "%Y-%m-%dT%H:%M:%S.%f")
+            self.created_at = datetime.strptime(kwargs['created_at'],
+                                                "%Y-%m-%dT%H:%M:%S.%f")
         if 'updated_at' in kwargs:
-            self.updated_at = datetime.strptime(kwargs['updated_at'], "%Y-%m-%dT%H:%M:%S.%f")
+            self.updated_at = datetime.strptime(kwargs['updated_at'],
+                                                "%Y-%m-%dT%H:%M:%S.%f")
 
-    # Method to update the instance with the provided attribute dictionary
     def update(self, attribute_dict):
+        """Update the instance with the
+        provided attribute dictionary"""
         for key, value in attribute_dict.items():
             setattr(self, key, value)
         self.save()
 
-    # String representation of the instance
     def __str__(self):
+        """Returns the string representation of the instance"""
         cls = type(self).__name__
         return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
 
-    # Convert instance into dict format
     def to_dict(self, secure_pwd=True):
+        """Converts instance into dict format"""
         new_dict = self.__dict__.copy()
         format_t = "%Y-%m-%dT%H:%M:%S.%f"
         dT = datetime
@@ -65,13 +69,38 @@ class BaseModel(Base):
             del new_dict['password']
         return new_dict
 
-    # Method to save the instance
+    @classmethod
+    def reload(cls):
+        """Deserializes the JSON file to instances"""
+        file_path = "file.json"
+        format_created = datetime.strptime(obj_data['created_at'],
+                                           "%Y-%m-%dT%H:%M:%S.%f")
+        format_updated = datetime.strptime(obj_data['updated_at'],
+                                           "%Y-%m-%dT%H:%M:%S.%f")
+        if os.path.exists(file_path):
+            with open(file_path, 'r') as file:
+                data = json.load(file)
+                for obj_id, obj_data in data.items():
+                    if obj_data['__class__'] == cls.__name__:
+                        del obj_data['__class__']
+                        obj_data['created_at'] = format_created
+                        obj_data['updated_at'] = format_updated
+                        if 'password' in obj_data:
+                            obj_data['password'] = md5(
+                                obj_data['password'].encode()
+                                ).hexdigest()
+                        instance = cls(**obj_data)
+                        setattr(cls, obj_id, instance)
+
     def save(self):
+        """Updates the updated_at and serializes
+        instances to a JSON file
+        """
         if models.storage_type != 'db':
             self.updated_at = datetime.now()
         models.storage.new(self)
         models.storage.save()
 
-    # Method to delete the instance from the storage
     def delete(self):
+        """Delete the current instance from the storage"""
         models.storage.delete(self)
